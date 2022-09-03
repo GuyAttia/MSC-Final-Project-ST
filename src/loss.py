a186dea89f6be33e76ea4381b7b3a7858724e2fc
Guy Attia <31534531+GuyAttia@users.noreply.github.com>
2022-09-03 18:53:07 +0300

    Add loss with spatial expression diff

--- src/loss.py
+++ src/loss.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+import pandas as pd
+import numpy as np
 
 
 # --------------------------------------------------- Losses ---------------------------------------------------------------------
@@ -43,16 +45,52 @@ class NON_ZERO_RMSELoss_Spatial(nn.Module):
     Implement another RMSE loss that clear the zero indices while adding regularization of the spatial distance
     """
 
-    def __init__(self, eps=1e-6):
+    def __init__(self, eps=1e-6, df_spots_neighbors=None, alpha=0.1, beta=0.1):
         super().__init__()
         self.mse = nn.MSELoss()
         self.eps = eps  # Add eps to avoid devision by 0
+        self.df_spots_neighbors = df_spots_neighbors
+        self.alpha = alpha
+        self.beta = beta
 
-    def forward(self, yhat, y, **kwargs):
+    def forward(self, yhat, y, spots, genes, **kwargs):
         # Create mask for all non zero items in the tensor
         non_zero_mask = torch.nonzero(y, as_tuple=True)
         y_non_zeros = y[non_zero_mask]  # Keep only non zero in y
         yhat_non_zeros = yhat[non_zero_mask]    # Keep only non zero in y_hat
 
         loss = torch.sqrt(self.mse(yhat_non_zeros, y_non_zeros) + self.eps)
+
+        # Spatial loss
+        df = pd.DataFrame({'gene': genes, 'spot': spots, 'yhat': yhat.detach()})
+
+        first_degree_loss = 0
+        second_degree_loss = 0
+
+        for i, (gene, spot, yhat_) in df.iterrows():
+            first_degree_neighbors = self.df_spots_neighbors.iloc[int(spot)][self.df_spots_neighbors.iloc[int(spot)]==1].index.values.astype(int)
+            second_degree_neighbors = self.df_spots_neighbors.iloc[int(spot)][self.df_spots_neighbors.iloc[int(spot)]==2].index.values.astype(int)
+
+            mask_spot = df['spot'] == spot
+            for spot_gene in df.loc[mask_spot, 'gene'].unique():
+                mask_gene = df['gene'] == spot_gene
+                # First degree loss
+                mask_neighbors = df['spot'].isin(first_degree_neighbors)
+                neighbors_expression = df.loc[mask_neighbors & mask_gene, 'yhat']
+                if len(neighbors_expression) > 0:
+                    part_loss = np.sqrt(np.mean(np.square(neighbors_expression - yhat_)))
+                    first_degree_loss += part_loss
+                    print(1)
+
+                # Second degree loss
+                mask_neighbors = df['spot'].isin(second_degree_neighbors)
+                neighbors_expression = df.loc[mask_neighbors & mask_gene, 'yhat']
+                if len(neighbors_expression) > 0:
+                    part_loss = np.sqrt(np.mean(np.square(neighbors_expression - yhat_)))
+                    first_degree_loss += part_loss
+                    print(2)
+
+
+        loss = loss + (self.alpha * first_degree_loss) + (self.beta * second_degree_loss)
+
         return loss
--- src/trainer.py
+++ src/trainer.py
@@ -8,6 +8,30 @@ from ignite.metrics import Loss
 
 from loss import *
 
+
+import torch.optim as optim
+from data import get_data
+from models import get_model
+import numpy as np
+import pandas as pd
+from os import path, listdir
+from copy import deepcopy
+import stlearn as st
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import matplotlib.pyplot as plt
+import cv2
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from torch import tensor
+from torch.utils.data import DataLoader, Dataset
+import torch.optim as optim
+from scanpy_stlearn_loaders import StlearnLoader
+import trainer as trainer
+from data import get_data
+
 # We used the Ignite package for smarter building of our trainers.
 # This package provide built-in loggers and handlers for different actions.
 
@@ -29,7 +53,7 @@ def trainer(model, optimizer, criterion, max_epochs, early_stopping, dl_train, d
 
         model.train()
         y_pred = model(gens, spots).to('cpu')
-        loss = criterion(y_pred, y)
+        loss = criterion(y_pred, y, spots, gens)
 
         # Backpropagation
         optimizer.zero_grad()
@@ -53,14 +77,17 @@ def trainer(model, optimizer, criterion, max_epochs, early_stopping, dl_train, d
             y_pred = model(gens, spots).to('cpu')
             return y_pred, y
 
-    val_metrics = {
-        "loss": Loss(criterion)
-    }
     train_evaluator = Engine(validation_step)
     val_evaluator = Engine(validation_step)
+
+    val_metrics = {
+        "loss": Loss(criterion),
+        'NonZeroRMSE': Loss(NON_ZERO_RMSELoss),
+    }
     # Attach metrics to the evaluators
     val_metrics['loss'].attach(train_evaluator, 'loss')
     val_metrics['loss'].attach(val_evaluator, 'loss')
+    val_metrics['NonZeroRMSE'].attach(val_evaluator, 'NonZeroRMSE')
 
     # Attach logger to print the training loss after each epoch
     @trainer.on(Events.EPOCH_COMPLETED)
@@ -76,7 +103,7 @@ def trainer(model, optimizer, criterion, max_epochs, early_stopping, dl_train, d
         val_evaluator.run(dl_test)
         metrics = val_evaluator.state.metrics
         print(
-            f"Validation Results - Epoch[{trainer.state.epoch}] Avg loss: {metrics['loss']:.2f}")
+            f"Validation Results - Epoch[{trainer.state.epoch}] Avg loss: {metrics['loss']:.2f}  |  Avg NonZeroRMSE: {metrics['NonZeroRMSE']:.2f}")
 
     def score_function(engine):
         """
@@ -138,26 +165,103 @@ def trainer(model, optimizer, criterion, max_epochs, early_stopping, dl_train, d
     return model, best_val_score
 
 
+
+class ExpressionDataset(Dataset):
+    """
+    Generate expression dataset to use in the our models, where each sample should be a tuple of (gene, spot, expression)
+    """
+
+    def __init__(self, df, device):
+        self.num_samples = len(df)
+        self.genes = tensor(df['gene'].values).to(device)
+        self.spots = tensor(df['spot'].values).to(device)
+        self.labels = tensor(df['expression'].values)
+        self.num_genes = df['gene'].max()
+        self.num_spots = df['spot'].max()
+
+    def __getitem__(self, index):
+        gene = self.genes[index]
+        spot = self.spots[index]
+        label = self.labels[index].item()
+        return gene, spot, label
+
+    def __len__(self):
+        return self.num_samples
+
+    def get_all_data(self):
+        return self.genes, self.spots, self.labels
+        
 # Only for testing
 if __name__ == '__main__':
-    import torch.optim as optim
-    from data import get_data
-    from models import get_model
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    dataset_name = 'V1_Human_Lymph_Node'
+    dataset_name = 'Visium_Mouse_Olfactory_Bulb'
     max_epochs = 2
+    early_stopping = 15
     model_name = 'NMF'
     best_params = {
-        'learning_rate': 0.001,
+        'learning_rate': 0.01,
         'optimizer': "RMSprop",
-        'latent_dim': 20,
-        'batch_size': 512
+        'latent_dim': 40,
+        'batch_size': 128
     }
 
-    dl_train, _, dl_test = get_data(dataset_name=dataset_name, batch_size=best_params['batch_size'], device=device)  # Get data
+    obj = StlearnLoader().load_local_visum(path=path.join('/', 'data', dataset_name), count_file='filtered_feature_bc_matrix.h5')
+    obj.layers["raw_count"] = obj.X
+    x = obj.X.toarray()
+    n_spots, n_genes = x.shape
+
+    # Keep genes with at least 15% non zero spots
+    min_cells = int(n_spots * 0.985)
+    st.pp.filter_genes(obj, min_cells=min_cells)
+    x = obj.X.toarray()
+    n_spots, n_genes = x.shape
+
+    obj.raw = obj
+    x = np.log(x+1)
+    
+    spots_values = obj.obs.index.values
+    genes_values = obj.var.index.values
+    df_expressions_matrix = pd.DataFrame(x, columns=genes_values, index=spots_values)
+    df_expressions = df_expressions_matrix.stack().reset_index()
+    df_expressions.columns = ['spot', 'gene', 'expression']
+
+    # Ordinal encoding the genes and spots for supported type
+    oe_genes = OrdinalEncoder()
+    df_expressions[['gene']] = oe_genes.fit_transform(df_expressions[['gene']].values)
+    oe_spots = OrdinalEncoder()
+    df_expressions[['spot']] = oe_spots.fit_transform(df_expressions[['spot']].values)
+    df_expressions[['spot', 'gene']] = df_expressions[['spot', 'gene']].astype(int)
+
+    df_train, df_test = train_test_split(df_expressions, test_size=0.10)
+    df_train, df_valid = train_test_split(df_train, test_size=0.10)
+
+    ds_train = ExpressionDataset(df=df_train, device=device)
+    ds_valid = ExpressionDataset(df=df_valid, device=device)
+    ds_test = ExpressionDataset(df=df_test, device=device)
+    
+    batch_size = 128
+    dl_train = DataLoader(dataset=ds_train, batch_size=batch_size, shuffle=True)
+    dl_valid = DataLoader(dataset=ds_valid, batch_size=batch_size, shuffle=True)
+    dl_test = DataLoader(dataset=ds_test, batch_size=batch_size, shuffle=True)
+
+    # dl_train, _, dl_test = get_data(dataset_name=dataset_name, batch_size=best_params['batch_size'], device=device)  # Get data
     model = get_model(model_name, best_params, dl_train)  # Build model
     optimizer = getattr(optim, best_params['optimizer'])(model.parameters(), lr=best_params['learning_rate'])  # Instantiate optimizer
-    criterion = NON_ZERO_RMSELoss()
+    df_spots_neighbors = pd.read_csv(path.join('/', 'FPST', 'src', 'spots_neighbors.csv'))
+    criterion = NON_ZERO_RMSELoss_Spatial(df_spots_neighbors=df_spots_neighbors, alpha=0.1, beta=0.1)
+    # criterion = NON_ZERO_RMSELoss()
     # criterion = RMSELoss()
-    test_loss = trainer(model, optimizer, criterion, max_epochs, dl_train, dl_test, device, dataset_name, model_name=model_name)
+    test_loss = trainer(
+                    model=model, 
+                    optimizer=optimizer, 
+                    criterion=criterion, 
+                    max_epochs=max_epochs, 
+                    early_stopping=early_stopping, 
+                    dl_train=dl_train, 
+                    dl_test=dl_valid, 
+                    device=device, 
+                    dataset_name=dataset_name, 
+                    model_name=model_name
+                )
+                
